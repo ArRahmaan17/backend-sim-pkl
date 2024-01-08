51781e47da4c266f0ba8abdd444ed666cfdf723a
RahmaanM17 <ardrah17@gmail.com>
2024-01-08 16:11:30 +0700

    feat: login and registration (btw its my birthday and i am still code for my application, so dont be a developer if you can't differentiate between professionalism and social activities)

--- routes/Users.js
+++ routes/Users.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const fs = require('fs')
 const router = express.Router();
-const { users } = require('../models')
+const bcrypt = require('bcrypt')
+const { users, attendances } = require('../models')
 let folder = 'storage/profile'
-const mutler = require('multer')
+const mutler = require('multer');
+const { sign } = require('jsonwebtoken');
 const storage = mutler.diskStorage({
     destination: (req, file, cb) => {
         if (file.fieldname == 'profile_picture') {
@@ -36,10 +38,34 @@ router.get('/:id', async (req, res) => {
     res.json({ 'status': 'success', 'message': 'user id ' + req.params.id, 'data': user ?? {} });
 });
 
-router.post('/', async (req, res) => {
-    const user = req.body;
-    const post = await users.create(user);
-    res.json({ 'status': 'success', 'data': post });
+router.post('/auth/registration', async (req, res) => {
+    const { username, password, email, phone_number } = req.body;
+    bcrypt.hash(password, 10).then(async (hashPassword) => {
+        const post = await users.create({
+            username: username,
+            password: hashPassword,
+            email: email,
+            phone_number: phone_number,
+        });
+        res.json({ 'status': 'success', 'data': post });
+    })
+});
+
+router.post('/auth/login', async (req, res) => {
+    const requestUser = req.body;
+    const user = await users.findOne({ where: { username: requestUser.username } });
+    if (!user) {
+        res.json({ status: 'failed', message: 'username doest match to our records', data: [] }, 404);
+    } else {
+        bcrypt.compare(requestUser.password, user.password).then((match) => {
+            if (!match) {
+                res.json({ status: 'failed', message: 'wrong username and password combination', data: [] }, 404);
+            } else {
+                const accessToken = sign({ username: user.username, id: user.id }, 'mamanrecing')
+                res.json({ status: 'success', message: 'successfully logged in', data: { accessToken: accessToken }, }, 200);
+            }
+        })
+    }
 })
 router.post('/attendance', async (req, res) => {
     let buffer = Buffer.from(req.body.photo, 'base64');
@@ -47,7 +73,14 @@ router.post('/attendance', async (req, res) => {
         fs.mkdirSync(`storage/attendances/${req.body.username}`)
     }
     const date = new Date;
-    fs.writeFileSync(`storage/attendances/${req.body.username}/attendance_${req.body.username}_${req.body.status}_${(date.toDateString()).split(' ').join('_')}.jpg`, buffer)
+    const filename = `storage/attendances/${req.body.username}/attendance_${req.body.username}_${req.body.status}_${(date.toDateString()).split(' ').join('_')}.jpg`;
+    fs.writeFileSync(filename, buffer);
+    const { latitude, longitude } = JSON.parse(req.body.location);
+    const attendance = await attendances.create({
+        status: req.body.status,
+        photo: filename,
+        location: `${latitude},${longitude}`,
+    });
     res.json({ message: `Attendance User ${req.body.username} successfully` });
 })
 router.post('/:id', upload.single('profile_picture'), async (req, res) => {
