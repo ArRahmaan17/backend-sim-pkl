bf334008bd1a228f540ed2dedb1e7d51298ad19b
RahmaanM17 <ardrah17@gmail.com>
2024-02-01 16:51:00 +0700

    feat: socket for chats

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
 app.use('/storage', express.static('storage/'))
-
+const { Server } = require('socket.io')
+const { createServer } = require('http')
 const db = require('./models');
 // route Application
 const UserRoute = require('./routes/Users');
 const AuthRoute = require('./routes/Auth');
 const MentorRoute = require('./routes/Mentor');
+const httpServer = createServer(app)
+const io = new Server(httpServer, { cors: { origin: "http://localhost:3000" } });
 app.use('/users', UserRoute);
 app.use('/auth', AuthRoute);
 app.use('/mentor', MentorRoute);
-
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log('Server Is Running http://localhost:3001');
+io.on("connection", (socket) => {
+    console.log(socket.id)
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit('message_received', data);
     });
-})
+});
+db.sequelize.sync().then(() => {
+    httpServer.listen(3001);
+});
