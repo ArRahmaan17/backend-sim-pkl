6d2fdb54a476e9b14ab0fc71097a009f278c45e8
RahmaanM17 <ardrah17@gmail.com>
2024-02-02 23:14:26 +0700

    feat: socket session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 const bodyParser = require('body-parser');
+const session = require("express-session");
 app.use(cors());
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
@@ -18,11 +19,23 @@ const io = new Server(httpServer, { cors: { origin: "http://localhost:3000" } })
 app.use('/users', UserRoute);
 app.use('/auth', AuthRoute);
 app.use('/mentor', MentorRoute);
+
+
+io.engine.use(session({
+    secret: "mamanrecing",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
 io.on("connection", (socket) => {
     console.log(socket.id)
     socket.on("send_message", (data) => {
         socket.broadcast.emit('message_received', data);
     });
+    socket.on("disconnected", () => {
+        console.log('socket disconnected');
+    })
 });
 db.sequelize.sync().then(() => {
     httpServer.listen(3001);
