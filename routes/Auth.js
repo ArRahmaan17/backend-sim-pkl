729e59bd44ed5691fc89f81726d8244d60c23d10
RahmaanM17 <ardrah17@gmail.com>
2024-06-12 17:14:04 +0700

    feat: chats app

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,23 +1,25 @@
 const express = require('express')
+const moment = require('moment')
 const router = express.Router();
 const bcrypt = require('bcrypt')
 const { sign } = require('jsonwebtoken');
 const { users, attendances } = require('../models')
+const salt = 10;
 router.post('/registration', async (req, res) => {
     const { username, password, email, phone_number } = req.body;
-    bcrypt.hash(password, 10).then(async (hashPassword) => {
-        try {
+    bcrypt.hash(password, salt, async function (err, hash) {
+        if (err) {
+            res.status(406).json({ 'status': 'failed', 'errors': errors });
+        } else {
             const post = await users.create({
                 username: username,
-                password: hashPassword,
+                password: hash,
                 email: email,
                 phone_number: phone_number,
             });
             res.status(201).json({ 'status': 'success', 'data': post });
-        } catch ({ errors }) {
-            res.status(406).json({ 'status': 'failed', 'errors': errors });
         }
-    })
+    });
 });
 
 router.post('/login', async (req, res) => {
@@ -26,14 +28,14 @@ router.post('/login', async (req, res) => {
     if (!user) {
         res.status(404).json({ status: 'failed', message: 'username doest match to our records', data: [] });
     } else {
-        bcrypt.compare(requestUser.password, user.password).then((match) => {
-            if (!match) {
+        bcrypt.compare(requestUser.password, user.password, function (err, result) {
+            if (err) {
                 res.status(404).json({ status: 'failed', message: 'wrong username and password combination', data: [] });
             } else {
-                const accessToken = sign({ username: user.username, id: user.id, clusterId: user.clusterId, role: 'M' }, 'mamanrecing')
-                res.status(202).json({ status: 'success', message: 'successfully logged in', data: { accessToken: accessToken }, });
+                const accessToken = sign({ username: user.username, id: user.id, clusterId: user.clusterId, role: 'M', lifetime: moment().add(1, 'days').unix() }, 'mamanrecing')
+                res.status(202).json({ status: 'success', message: 'successfully logged in', data: { accessToken: accessToken } });
             }
-        })
+        });
     }
 });
 
